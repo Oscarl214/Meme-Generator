74214cf85d98e97ec897a65e0aa75632bd4a211d
Oscar2145 <74516013+Oscar2145@users.noreply.github.com>
2023-10-25 15:29:04 -0500

    touched up styling

--- client/src/components/Meme.js
+++ client/src/components/Meme.js
@@ -1,20 +1,47 @@
 import React from 'react';
 
+import memeData from '../memesData'
 const Generator = () => {
+
+function handleImage(){
+    // Access meme data from the imported JSON
+    const data = memeData;
+
+    // Generate a random index within the range of the memes array
+    const randomIndex = Math.floor(Math.random() * data.data.memes.length);
+
+    // Access the URL of the randomly selected meme
+    const randomMeme = data.data.memes[randomIndex];
+    const randomURL = randomMeme.url;
+
+    console.log("URL of image:", randomURL);
+
+
+}
+
+
   return (
-    <div>
-      <form>
-        <label>
-          <input type="text" />
-        </label>
-        <label>
-          <input type="text" />
-        </label>
-      </form>
-      <div className="button-container">
-        <button>Get a New Meme Image</button>
-      </div>
+    <main>
+    <div className="form">
+        <p></p>
+        <input 
+            type="text"
+            placeholder="Top text"
+            className="form--input"
+        />
+        <input 
+            type="text"
+            placeholder="Bottom text"
+            className="form--input"
+        />
+        <button 
+            className="form--button"
+            onClick={handleImage}
+        >
+            Get a new meme image 🖼
+        </button>
     </div>
+</main>
   );
 };
 
